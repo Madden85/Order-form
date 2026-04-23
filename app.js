f1c682114f17f2d437bdbe4f6fbc59c7ce3b08bd
Madden85 <mohdeizudinsyah@gmail.com>
2026-04-23 20:02:32 +0800

    Add files via upload

--- app.js
+++ app.js
@@ -4,6 +4,10 @@ const copyBtn = document.getElementById("copyBtn");
 const product = document.getElementById("product");
 const submitBtn = document.getElementById("submitBtn");
 
+/* 🔥 TELEGRAM SETUP */
+const BOT_TOKEN = "REMOVED";
+const CHAT_ID = "310295809";
+
 product.onchange = renderForm;
 
 function input(id,placeholder){
@@ -24,120 +28,7 @@ ${input("exp","Expired Date")}
 ${input("email","Email Address")}
 `;
 
-if(p.includes("netflix")){
-form.innerHTML = common + `
-${input("pass","Password")}
-${input("profile","Nama Profile")}
-${input("pin","Pincode")}
-<textarea readonly>
-⚠️ JANGAN UBAH apa2 setting
-⚠️ JANGAN KACAU profile lain
-1️⃣ HANYA 1 SCREEN SAHAJA pada satu2 masa
-
-p/s-Jika didapati buka lebih dari 1 screen dalam satu2 masa,PROFILE AKAN DINYAHAKTIF & TIADA REFUND ❗️Peringatan Bermula Julai 2025 kami dari pihak NuMo ventures akan menukar password netflix secara RANDOM bagi mengelakkan acc diceroboh pihak yang tidak bertanggungjawab
-</textarea>`;
-}
-
-else if(p.includes("own")){
 form.innerHTML = common;
-}
-
-else if(p.includes("seller")){
-form.innerHTML = common + `
-${input("pass","Password")}
-<textarea readonly>
-Note
-Hanya 1 device shj dibenarkan🚫
-
-🔷Sila masuk youtube, tekan add account, masuk detail yg diberi dan tekan log in
-
-🔷Selepas log in, tak boleh tukar password
-</textarea>`;
-}
-
-else if(p.includes("sooka")){
-form.innerHTML = common + `
-${input("pass","Password")}
-<textarea readonly>
-Note
-⚠️ Jangan ubah apa2 setting
-
-❌Boleh log in 1 device sahaja
-
-1️⃣ HANYA 1 screen sahaja pada satu2 masa
-
-p/s-Jika didapati buka lebih dari 1 DEVICE, Access akan dinyahaktifkan & tiada refund
-</textarea>`;
-}
-
-else if(p.includes("spotify")){
-form.innerHTML = common + `
-${input("link","Link Invitation")}
-<textarea readonly>
-Note
-1)Tekan link yang diberi dan tekan TERIMA JEMPUTAN
-
-2)DAFTAR AKAUN SPOTIFY atau LOG MASUK menggunakan akaun anda yang sedia ada
-
-3)Sahkan alamat anda - Lebuh Nipah 1
-
-4)Siap — biarkan muzik bermula.
-
- Lepas dah boleh join family, sila inform admin semula
-
- Hanya 1 DEVICE SAHAJA untuk 1 langganan
-</textarea>`;
-}
-
-else if(p.includes("iqiyi")){
-form.innerHTML = common + `
-${input("pass","Password")}
-<textarea readonly>
-Note
-⚠️ Jangan ubah apa2 setting
-
-❌Boleh log in 1 device sahaja
-
-1️⃣ HANYA 1 screen sahaja pada satu2 masa
-
-p/s-Jika didapati buka lebih dari 1 screen dalam satu2 masa, profile akan dinyahaktifkan & tiada refund
-</textarea>`;
-}
-
-else if(p.includes("disney")){
-form.innerHTML = common + `
-${input("profile","Nama Profile")}
-${input("phone","Phone Number")}
-<textarea readonly>
-Note
-1)Buka app Disney+ Hotstar
-
-2)Masukkan no phone
-
-3)Masukkan code yang admin akan bagi
-
-4)Siap
-
-Hanya 1 DEVICE SAHAJA untuk 1 langganan
-
-JANGAN GANGGU PROFILE ORANG LAIN
-</textarea>`;
-}
-
-else if(p.includes("viu")){
-form.innerHTML = common + `
-${input("pass","Password")}
-<textarea readonly>
-Note
-⚠️ Jangan ubah apa2 setting
-
-❌Boleh log in 1 device sahaja
-
-1️⃣ HANYA 1 screen sahaja pada satu2 masa
-
-p/s-Jika didapati buka lebih dari 1 screen dalam satu2 masa, profile akan dinyahaktifkan & tiada refund
-</textarea>`;
-}
 }
 
 submitBtn.onclick = generate;
@@ -158,26 +49,6 @@ Username: ${val("tg")}
 Email: ${val("email")}
 `;
 
-if(val("pass")) text+=`Password: ${val("pass")}\n`;
-if(val("profile")) text+=`Profile name: ${val("profile")}\n`;
-if(val("pin")) text+=`Pincode: ${val("pin")}\n`;
-if(val("phone")) text+=`Phone: ${val("phone")}\n`;
-if(val("link")) text+=`Link: ${val("link")}\n`;
-
-let note = document.querySelector("textarea")?.value || "";
-text += "\n\n" + note;
-
-text += `
-
--------------------------------------
-
-GENERATE CODE atau VERIFY EMAIL anda sendiri disini
-
-❗PENTING❗ Sila gunakan order number yang diberikan di atas 👆 
-
-https://numoverifycode.netlify.app/
-`;
-
 /* OUTPUT */
 result.classList.remove("hidden");
 result.innerText = text;
@@ -185,7 +56,7 @@ result.innerText = text;
 /* AUTO COPY */
 navigator.clipboard.writeText(text);
 
-/* 🔥 AUTO SEND TELEGRAM */
+/* 🔥 SEND TO APPS SCRIPT */
 fetch("https://script.google.com/macros/s/AKfycbyRGzkQ-2BF2e_MvstkoBwnBmVt75rJ1EjBW2rKERzBvZYWTHIA0ZpwNhl2Rrou_7mq/exec",{
 method:"POST",
 headers:{"Content-Type":"application/json"},
@@ -198,7 +69,7 @@ exp:val("exp")
 })
 });
 
-alert("Order generated + sent to Telegram!");
+alert("Order generated + sent!");
 
 copyBtn.classList.remove("hidden");
 
